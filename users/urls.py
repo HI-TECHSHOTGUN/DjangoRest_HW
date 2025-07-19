a4f984f14974d09df37f9d1f05dad42a203548b8
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-07-19 20:28:41 +0300

    Not done

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from users.apps import UsersConfig
+from .views import PaymentViewSet, UserViewSet
+
+app_name = UsersConfig.name
+
+router = DefaultRouter()
+router.register(r'payments', PaymentViewSet, basename='payments')
+router.register(r'users', UserViewSet, basename='users')
+
+urlpatterns = [] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Payment, User
+from .serializers import PaymentSerializer, UserSerializer
+
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """ ViewSet для модели Пользователя """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
